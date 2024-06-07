713aecb9cd4f36631a461e643ee0eed78dcfdd93
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-07 06:33:18 +0200

    added docs

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,7 @@
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json' assert { type: 'json' };
 import express from 'express';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json' assert { type: 'json' };
 const docRouter = express.Router();
 docRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
