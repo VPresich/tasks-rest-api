f97021dc2c7da67881e88128bee5e164d5916b40
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-07 07:33:34 +0200

    swagger oтключен

--- routes/docs.js
+++ routes/docs.js
@@ -1,8 +1,8 @@
-import express from 'express';
+// import express from 'express';
 
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json' assert { type: 'json' };
-const docRouter = express.Router();
-docRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerDocument from '../swagger.json' assert { type: 'json' };
+// const docRouter = express.Router();
+// docRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-export default docRouter;
+// export default docRouter;
